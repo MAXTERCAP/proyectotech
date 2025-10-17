923eba809a6bfa71bfd154e4c5eb3d9e233f425f
Maximiliano Bollini <maxbollini@hotmail.com>
2025-10-17 20:54:06 -0300

    Primeros cambios básicos 1.0

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+const Header = () => {
+  return (
+    <Navbar style= {{ backgroundColor: '#73683d' }}  variant="dark" expand="lg" className="mb-4">
+      <Container>       
+        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+          <img
+            src="./logo/logo.jpg" 
+            alt="Logo"
+            className="d-inline-block align-top me-2"
+          />
+          <span>Proyecto Tech - Los mejores precios</span>
+        </Navbar.Brand>
+
+        <Nav className="ms-auto align-items-center">
+          <Nav.Link as={Link} to="/" className="me-3">Home</Nav.Link>
+          <Nav.Link as={Link} to="/ofertas" className="me-3">Ofertas</Nav.Link>
+          <Nav.Link as={Link} to="/infaltables" className="me-3">Liquidación</Nav.Link>
+
+          <div className="d-flex align-items-center">
+            <Button variant="outline-light" as={Link} to="/administracion" className="me-2">
+              Administración
+            </Button>
+            <Link to="/carrito" className="text-white">
+              <FontAwesomeIcon icon={faShoppingCart} size="lg" />
+            </Link>
+          </div>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
